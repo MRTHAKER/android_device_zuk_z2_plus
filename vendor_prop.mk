6fea0e45c6b3be43871ccb392a3e07fb23c18dcc
Manu Prasad <mpras@codeaurora.org>
2018-06-28 12:28:24 +0530

    z2_plus: Renamed video.disable.ubwc
    Updating property name. Renamed video.disable.ubwc to
    vendor.video.disable.ubwc
    
    Change-Id: Ie96384988317ec5612cadc57aa9e2a586c34745d

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -77,7 +77,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.enable_default_color_mode=1 \
     vendor.display.disable_skip_validate=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
-    video.disable.ubwc=1
+    vendor.video.disable.ubwc=1
 
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
