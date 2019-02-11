6e24bee7e1012288a6553427874686550fbb04db
Michael Bestas <mkbestas@lineageos.org>
2019-02-11 15:35:46 +0200

    z2_plus: Stop copying WCNSS_qcom_cfg.ini to userdata
    Change-Id: I7b9fbc0e26a1563f3e13fe0f9d58308b618a5702

--- device.mk
+++ device.mk
@@ -405,7 +405,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_OUT_VENDOR)/etc/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_OUT_VENDOR)/etc/firmware/wlan/qca_cld/WCNSS_cfg.dat
 
 # Call the proprietary setup
